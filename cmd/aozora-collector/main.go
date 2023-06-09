eff9cc3332da0977a4c09f7d7becf9506872b98e
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-06-09 10:42:05 +0900

    zipファイルを展開する

--- cmd/aozora-collector/main.go
+++ cmd/aozora-collector/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
 	"path"
 	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/text/encoding/japanese"
 )
 
 type Entry struct {
@@ -87,6 +93,50 @@ func findEntries(siteURL string) ([]Entry, error) {
 	return entries, nil
 }
 
+func extractText(zipURL string) (string, error) {
+	// URLからHTTP GETリクエストを送信し、レスポンスを取得する
+	resp, err := http.Get(zipURL)
+	if err != nil {
+		return "", err
+	}
+	// deferの後に続く関数の実行を、現在の関数が終了する直前まで遅らせる
+	// 現在の関数が終了する直前に、resp.Body.Close()を実行して、HTTPレスポンスのボディをクローズする
+	// これを怠ると、ネットワークリソースが不適切に保持され続け、リソースリークの原因となる可能性がある
+	defer resp.Body.Close()
+
+	// HTTPレスポンスのボディ部分を全て読み込みます
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	// 読み込んだZIPファイルの内容をメモリ上に展開
+	r, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
+	// 展開した各ファイルをループで処理します。このループの中で.txtファイルを探しています
+	for _, file := range r.File {
+		if path.Ext(file.Name) == ".txt" {
+			// .txtファイルを開く
+			f, err := file.Open()
+			if err != nil {
+				return "", err
+			}
+			// 全て読み込みます
+			b, err := ioutil.ReadAll(f)
+			f.Close()
+			if err != nil {
+				return "", err
+			}
+			// 読み込んだ.txtファイルの内容をShift-JISからUTF-8に変換します
+			b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
+			if err != nil {
+				return "", err
+			}
+			// 変換したテキスト内容を文字列として返します
+			return string(b), nil
+		}
+	}
+	return "", errors.New("contents not found")
+}
+
 func main() {
 	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
 	entries, err := findEntries(listURL)
@@ -95,6 +145,12 @@ func main() {
 	}
 
 	for _, entry := range entries {
-		fmt.Println(entry.Title, entry.ZipURL)
+		content, err := extractText(entry.ZipURL)
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		fmt.Println(entry.SiteURL)
+		fmt.Println(content)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/shimabukuromeg/meg-aozora-search
 
 go 1.20
 
+require golang.org/x/text v0.9.0
+
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
