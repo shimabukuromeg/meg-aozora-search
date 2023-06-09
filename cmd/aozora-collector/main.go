201ec28a429c594017e4549ac856edea73c3448b
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-06-09 10:24:09 +0900

    zipファイルのURL一覧を取得

---
+++ cmd/aozora-collector/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Entry struct {
+	AuthorID string
+	Author   string
+	TitleID  string
+	Title    string
+	SiteURL  string
+	ZipURL   string
+}
+
+// 作者とZIPファイルのURLを取得する
+func findAuthorAndZIP(siteURL string) (string, string) {
+	// log.Println("query", siteURL)
+	doc, err := goquery.NewDocument(siteURL)
+	if err != nil {
+		return "", ""
+	}
+	author := doc.Find("table[summary=作家データ] tr:nth-child(2) td:nth-child(2)").First().Text()
+	zipURL := ""
+	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
+		href := elem.AttrOr("href", "")
+		if strings.HasSuffix(href, ".zip") {
+			zipURL = href
+		}
+	})
+
+	if zipURL == "" {
+		return author, ""
+	}
+
+	// zip url が最初から絶対パスで指定されている場合はそのまま返す
+	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "https://") {
+		return author, zipURL
+	}
+
+	u, err := url.Parse(siteURL)
+	if err != nil {
+		return author, ""
+	}
+	u.Path = path.Join(path.Dir(u.Path), zipURL)
+	return author, u.String()
+}
+
+var pageURLFormat = "https://www.aozora.gr.jp/cards/%s/card%s.html"
+
+func findEntries(siteURL string) ([]Entry, error) {
+	// TODO: Implement
+	// goqueryでURLからDOMオブジェクトを取得する
+	doc, err := goquery.NewDocument(siteURL)
+	if err != nil {
+		return nil, err
+	}
+	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
+	entries := []Entry{}
+	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
+		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
+		if len(token) != 3 {
+			return
+		}
+		title := elem.Text()
+		pageURL := fmt.Sprintf(pageURLFormat, token[1], token[2])
+		author, zipURL := findAuthorAndZIP(pageURL)
+		if zipURL != "" {
+			entries = append(entries, Entry{
+				AuthorID: token[1],
+				Author:   author,
+				TitleID:  token[2],
+				Title:    title,
+				SiteURL:  siteURL,
+				ZipURL:   zipURL,
+			})
+		}
+	})
+
+	return entries, nil
+}
+
+func main() {
+	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
+	entries, err := findEntries(listURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, entry := range entries {
+		fmt.Println(entry.Title, entry.ZipURL)
+	}
+}
